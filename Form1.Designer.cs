2fb74ea333339708f7b8c54fdb6a06be2d041b7b
TinUIT <89720388+TinUIT@users.noreply.github.com>
2022-06-06 21:48:53 +0700

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,233 @@
+﻿namespace Lab4_mmh_task1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rtbData = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.tbMD5 = new System.Windows.Forms.TextBox();
+            this.tbSHA1 = new System.Windows.Forms.TextBox();
+            this.tbSHA256 = new System.Windows.Forms.TextBox();
+            this.tbSHA512 = new System.Windows.Forms.TextBox();
+            this.cbFomat = new System.Windows.Forms.ComboBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.tbFile = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // rtbData
+            // 
+            this.rtbData.Location = new System.Drawing.Point(155, 41);
+            this.rtbData.Name = "rtbData";
+            this.rtbData.Size = new System.Drawing.Size(835, 56);
+            this.rtbData.TabIndex = 0;
+            this.rtbData.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(150, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 29);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Data";
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalculate.Location = new System.Drawing.Point(421, 406);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(141, 44);
+            this.btnCalculate.TabIndex = 2;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(5, 186);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 29);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "MD5";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(5, 238);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(74, 29);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "SHA1";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(5, 296);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(100, 29);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "SHA256";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(5, 345);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(121, 29);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "SHA3-512";
+            // 
+            // tbMD5
+            // 
+            this.tbMD5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbMD5.Location = new System.Drawing.Point(155, 186);
+            this.tbMD5.Name = "tbMD5";
+            this.tbMD5.Size = new System.Drawing.Size(835, 30);
+            this.tbMD5.TabIndex = 7;
+            // 
+            // tbSHA1
+            // 
+            this.tbSHA1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbSHA1.Location = new System.Drawing.Point(155, 238);
+            this.tbSHA1.Name = "tbSHA1";
+            this.tbSHA1.Size = new System.Drawing.Size(835, 30);
+            this.tbSHA1.TabIndex = 8;
+            // 
+            // tbSHA256
+            // 
+            this.tbSHA256.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbSHA256.Location = new System.Drawing.Point(155, 296);
+            this.tbSHA256.Name = "tbSHA256";
+            this.tbSHA256.Size = new System.Drawing.Size(835, 30);
+            this.tbSHA256.TabIndex = 9;
+            // 
+            // tbSHA512
+            // 
+            this.tbSHA512.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbSHA512.Location = new System.Drawing.Point(155, 345);
+            this.tbSHA512.Multiline = true;
+            this.tbSHA512.Name = "tbSHA512";
+            this.tbSHA512.Size = new System.Drawing.Size(835, 26);
+            this.tbSHA512.TabIndex = 10;
+            // 
+            // cbFomat
+            // 
+            this.cbFomat.FormattingEnabled = true;
+            this.cbFomat.Location = new System.Drawing.Point(10, 41);
+            this.cbFomat.Name = "cbFomat";
+            this.cbFomat.Size = new System.Drawing.Size(109, 28);
+            this.cbFomat.TabIndex = 11;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(5, 9);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(81, 29);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Fomat";
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBrowse.Location = new System.Drawing.Point(7, 125);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(112, 44);
+            this.btnBrowse.TabIndex = 13;
+            this.btnBrowse.Text = "Browse";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // tbFile
+            // 
+            this.tbFile.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbFile.Location = new System.Drawing.Point(155, 132);
+            this.tbFile.Name = "tbFile";
+            this.tbFile.Size = new System.Drawing.Size(835, 30);
+            this.tbFile.TabIndex = 14;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1011, 460);
+            this.Controls.Add(this.tbFile);
+            this.Controls.Add(this.btnBrowse);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.cbFomat);
+            this.Controls.Add(this.tbSHA512);
+            this.Controls.Add(this.tbSHA256);
+            this.Controls.Add(this.tbSHA1);
+            this.Controls.Add(this.tbMD5);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.rtbData);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox rtbData;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox tbMD5;
+        private System.Windows.Forms.TextBox tbSHA1;
+        private System.Windows.Forms.TextBox tbSHA256;
+        private System.Windows.Forms.TextBox tbSHA512;
+        private System.Windows.Forms.ComboBox cbFomat;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.TextBox tbFile;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Security.Cryptography;
+using System.IO;
+
+namespace Lab4_mmh_task1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            cbFomat.Items.Add("Text");
+            cbFomat.Items.Add("Hex");
+            cbFomat.Items.Add("File");
+
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            string data = rtbData.Text;
+            byte[] input = null;
+
+            if(cbFomat.Text == "")
+            {
+                MessageBox.Show("Please choose format","Error",MessageBoxButtons.OK,MessageBoxIcon.Error);
+                return;
+            }
+            if(cbFomat.Text == "Hex")
+            {
+                input = StringToByteArray(data);
+            }
+            else if (cbFomat.Text == "File")
+            {
+                if(tbFile.Text != "")
+                    input = File.ReadAllBytes(tbFile.Text);
+                else
+                {
+                    MessageBox.Show("Please choose file", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+            }
+
+            tbMD5.Text = MD5(data,input);
+            tbSHA1.Text = SHA1(data, input);
+            tbSHA256.Text = SHA256(data, input);
+            tbSHA512.Text = SHA3_512(data, input);
+        }
+
+        public static byte[] StringToByteArray(string hex)
+        {
+            return Enumerable.Range(0, hex.Length)
+                             .Where(x => x % 2 == 0)
+                             .Select(x => Convert.ToByte(hex.Substring(x, 2), 16))
+                             .ToArray();
+        }
+
+        public string byteToString (byte[] result)
+        {
+            StringBuilder strBuilder = new StringBuilder();
+            for (int i = 0; i < result.Length; i++)
+            {
+                strBuilder.Append(result[i].ToString("x2"));
+            }
+
+            return strBuilder.ToString();
+        }
+
+        public string MD5(string plainText="", byte[] hex = null)
+        {
+            MD5 md5 = new MD5CryptoServiceProvider();
+
+            if (hex == null)
+            {
+                md5.ComputeHash(ASCIIEncoding.ASCII.GetBytes(plainText));
+            }
+            else
+            {
+                md5.ComputeHash(hex);
+            }
+                     
+            byte[] result = md5.Hash;
+            return byteToString(result);
+        }
+
+        public string SHA1(string plainText = "", byte[] hex = null)
+        {
+            SHA1 sha1 = new SHA1CryptoServiceProvider();
+
+            if (hex == null)
+            {
+                sha1.ComputeHash(ASCIIEncoding.ASCII.GetBytes(plainText));
+            }
+            else
+            {
+                sha1.ComputeHash(hex);
+            }
+
+            byte[] result = sha1.Hash;
+            return byteToString(result);
+        }
+
+        public string SHA256(string plainText = "", byte[] hex = null)
+        {
+            SHA256 sha256 = new SHA256CryptoServiceProvider();
+
+            if (hex == null)
+            {
+                sha256.ComputeHash(ASCIIEncoding.ASCII.GetBytes(plainText));
+            }
+            else
+            {
+                sha256.ComputeHash(hex);
+            }
+
+            byte[] result = sha256.Hash;
+            return byteToString(result);
+        }
+
+        public string SHA3_512(string plainText = "", byte[] hex = null)
+        {
+            var hashAlgorithm = new Org.BouncyCastle.Crypto.Digests.Sha3Digest(512);
+
+            // Choose correct encoding based on your usecase
+            byte[] input;
+            if (hex == null)
+            {
+                input = Encoding.ASCII.GetBytes(plainText);
+            }
+            else
+            {
+                input = hex;
+            }
+
+            hashAlgorithm.BlockUpdate(input, 0, input.Length);
+
+            byte[] result = new byte[64]; 
+            hashAlgorithm.DoFinal(result, 0);
+
+            string hashString = BitConverter.ToString(result);
+            return  hashString.Replace("-", "").ToLowerInvariant();
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.ShowDialog();
+            tbFile.Text = ofd.FileName.ToString();
+        }
+    }
+}
